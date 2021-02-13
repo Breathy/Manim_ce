2dc34ac7ac9d3ed7b19a9a95cef4cff3c017f6fa
Nassim Haidar <breathy@Nassims-MBP.fritz.box>
2021-02-13 16:27:19 +0100

    second

---
+++ Test.py
@@ -0,0 +1,13 @@
+from manim import *
+import numpy as np
+
+config["background_color"]=GREY
+
+class Test(Scene):
+	def construct(self):
+		VGroup=
+		
+		ci=Circle(stroke_color= YELLOW_A).shift(LEFT*3)
+		self.play(ShowCreation(ci))
+		
+
